c6f3c27a1035cfbc6d1b92841fea508bcbe6b3e5
sandraortiz <rocio12vx@gmail.com>
2018-08-09 21:15:50 -0500

    traendo data del los restaurantes

--- src/main.js
+++ src/main.js
@@ -1,40 +1,41 @@
-const  restaurantname =  document.getElementById('name')
-const  restauranoptio = document.getElementById("restauranOption")
-const datafile1 = '../data/restaurant.json'
-const llamadas = [];
-let restaurant= [];
-llamadas.push(fetch(datafile1));
-Promise.all(llamadas)
-  .then(
-    response => Promise.all(
-      response.map(
-        data => data.text()
-      )
-    )
-  )
-  .then(
-    response => {
-     restaurants= JSON.parse(response[0])
-     allRestaurant();
-      console.log(restaurants);
-    }
-  );
-  const allRestaurant  = () => {
-  restaurants.forEach(restaurant => {
-    // const restaurantName = (restaurant.image)
-    // console.log(restaurantName);
-    const containerRestauran= document.createElement('div');
-    containerRestauran.setAttribute
-    const container = document.createElement('div');
-    container.setAttribute('class', "w3-container w3-white" )
-    const image =  document.createElement('img');
-    image.setAttribute('class' , "w3-round-large");
-    image.setAttribute('style' , "width:50%");
-    image.setAttribute('src', `${restaurant.image}` )
-   restauranoptio.appendChild(container);
-    container.appendChild(image);
-
-    // restaurantname.innerHTML +=`<img src="${restaurant.image}" alt="user" class="profile-photo" />`;
+const restaurantname = document.getElementById('name')
+const restauranoptio = document.getElementById("restauranOption");
+fetch('../data/restaurant.json')
+  .then(response => response.json())
+  .then(restaurants => {
+    restaurants.forEach(restaurant => {
+      // const nameRest = restaurant.image;
+      const content = document.createElement('div');
+      content.setAttribute('class', "w3-third w3-margin-bottom");
+      const imagerest = document.createElement('img');
+      imagerest.setAttribute( 'src' , `${restaurant.image} `)
+      imagerest.setAttribute('class' , "w3-round-large")
+      imagerest.setAttribute('style', "width:60%" )
+      const buttonres = document.createElement('input');
+      buttonres.setAttribute('value','Ver' )
+      buttonres.setAttribute('type', 'button')
+      buttonres.setAttribute('id' , "hola" )
+     content.appendChild(imagerest);
+      content.appendChild(buttonres);
+      restauranoptio.appendChild(content);
+      })
   })
-  }
- 
+
+const allRestaurant = () => {
+  restaurants.forEach(restaurant => {
+    restauranoptio.innerHTML +=
+      `<div class="w3-third w3-margin-bottom" >
+    <img src="${restaurant.image}" style="width:80%"  "padding: 5px" width: 150px; class="w3-round-large" > 
+    <br>
+   <button class="w3-button w3-black w3-margin-bottom" id="information">Ver</button>
+    </div>`
+    const buttonmodal = document.getElementById('information');
+    buttonmodal.addEventListener('click', () => {
+      alert('hola');
+    })
+  })
+
+}
+
+
+
